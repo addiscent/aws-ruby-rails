69beaeef96c8c662e3d86d292a80874be48f08d0
Rex Addiscentis <raddiscentis@addiscent.com>
2016-07-11 22:27:00 -0700

    Update documentation, tweak .sh

--- ec2-provision-ruby-rails.sh
+++ ec2-provision-ruby-rails.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
-# install and execute script which provisions ruby/rails.
+# install and execute a script which provisions ruby/rails.
 # During launch of a new EC2 instance, copy-paste the code herein into
 # the "Advanced Details -> User Data" edit box
+# rex 2016.2133.00
 
 apt-get -y install unzip
 
@@ -14,5 +15,6 @@ chmod +x ./aws-ruby-rails-provision-master/aws-ruby-rails-provision.sh
 ./aws-ruby-rails-provision-master/aws-ruby-rails-provision.sh
 
 rm -r ./aws-ruby-rails-provision-master
+
 rm aws-ruby-rails-provision.zip
 
