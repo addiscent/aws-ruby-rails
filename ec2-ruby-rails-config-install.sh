8d8940cb3826e712989ae3d7ce1d00a95554e77c
Rex Addiscentis <raddiscentis@addiscent.com>
2016-07-15 18:30:01 -0700

    Move install unzip again, add double-amp so continues only after command success

--- ec2-ruby-rails-config-install.sh
+++ ec2-ruby-rails-config-install.sh
@@ -11,41 +11,41 @@ echo "BEGIN ec2-ruby-rails-config-install.sh" >> /home/ubuntu/arr-provision.log
 
 echo "ec2-ruby-rails-config-install.sh : Set hostname to aws-ruby-rails"
 
-hostname aws-ruby-rails
+hostname aws-ruby-rails && \
 
-echo "aws-ruby-rails" > /etc/hostname
+echo "aws-ruby-rails" > /etc/hostname && \
 
-sed -i 's/localhost/localhost aws-ruby-rails/g' /etc/hosts
+sed -i 's/localhost/localhost aws-ruby-rails/g' /etc/hosts && \
 
 #############################################################################
 # set custom prompt and functions/aliases in .bashrc for users root and ubuntu
 
 echo "ec2-ruby-rails-config-install.sh : Set .bashrc customization for root and ubuntu"
 
-cat ./aws-ruby-rails-master/bashrc-mod.txt >> /home/ubuntu/.bashrc
+cat ./aws-ruby-rails-master/bashrc-mod.txt >> /home/ubuntu/.bashrc && \
 
-cat ./aws-ruby-rails-master/bashrc-mod.txt >> /root/.bashrc
+cat ./aws-ruby-rails-master/bashrc-mod.txt >> /root/.bashrc && \
 
 ############################################################################
 # install ruby/rails and related tools/software
 
 echo "ec2-ruby-rails-config-install.sh : wget -O ruby-rails-install.zip https://github.com/addiscent/ruby-rails-install/archive/master.zip" >> /home/ubuntu/arr-provision.log
-wget -O ruby-rails-install.zip https://github.com/addiscent/ruby-rails-install/archive/master.zip
+wget -O ruby-rails-install.zip https://github.com/addiscent/ruby-rails-install/archive/master.zip && \
 
 echo "ec2-ruby-rails-config-install.sh : unzip ruby-rails-install.zip" >> /home/ubuntu/arr-provision.log
-unzip ruby-rails-install.zip
+unzip ruby-rails-install.zip && \
 
 echo "ec2-ruby-rails-config-install.sh : chmod +x ./ruby-rails-install-master/ruby-rails-install.sh" >> /home/ubuntu/arr-provision.log
-chmod +x ./ruby-rails-install-master/ruby-rails-install.sh
+chmod +x ./ruby-rails-install-master/ruby-rails-install.sh && \
 
 echo "ec2-ruby-rails-config-install.sh : ./ruby-rails-install-master/ruby-rails-install.sh" >> /home/ubuntu/arr-provision.log
-./ruby-rails-install-master/ruby-rails-install.sh >> /home/ubuntu/arr-provision.log
+./ruby-rails-install-master/ruby-rails-install.sh >> /home/ubuntu/arr-provision.log && \
 
 echo "ec2-ruby-rails-config-install.sh : rm -r ./ruby-rails-install-master" >> /home/ubuntu/arr-provision.log
-rm -r ./ruby-rails-install-master
+rm -r ./ruby-rails-install-master && \
 
 echo "ec2-ruby-rails-config-install.sh : rm ruby-rails-install.zip" >> /home/ubuntu/arr-provision.log
-rm ruby-rails-install.zip
+rm ruby-rails-install.zip && \
 
 echo "END ec2-ruby-rails-config-install.sh" >> /home/ubuntu/arr-provision.log
 
--- ec2-ruby-rails.sh
+++ ec2-ruby-rails.sh
@@ -6,29 +6,29 @@
 echo "BEGIN ec2-ruby-rails.sh" >> /home/ubuntu/arr-provision.log
 
 ############################################################################
+# install unzip
+echo "ec2-ruby-rails-config-install.sh : apt-get -y install unzip" >> /home/ubuntu/arr-provision.log
+apt-get -y install unzip && \
+
+############################################################################
 # download and execute provisioning software
 
 echo "ec2-ruby-rails.sh : wget -O aws-ruby-rails.zip https://github.com/addiscent/aws-ruby-rails/archive/master.zip" >> /home/ubuntu/arr-provision.log
 wget -O aws-ruby-rails.zip https://github.com/addiscent/aws-ruby-rails/archive/master.zip && \
 
-############################################################################
-# install unzip
-echo "ec2-ruby-rails-config-install.sh : apt-get -y install unzip" >> /home/ubuntu/arr-provision.log
-apt-get -y install unzip && \
-
-echo "ec2-ruby-rails.sh : unzip aws-ruby-rails.zip" >> /home/ubuntu/arr-provision.log && \
+echo "ec2-ruby-rails.sh : unzip aws-ruby-rails.zip" >> /home/ubuntu/arr-provision.log
 unzip aws-ruby-rails.zip && \
 
-echo "ec2-ruby-rails.sh : chmod +x ./aws-ruby-rails-master/ec2-ruby-rails-config-install.sh" >> /home/ubuntu/arr-provision.log && \
+echo "ec2-ruby-rails.sh : chmod +x ./aws-ruby-rails-master/ec2-ruby-rails-config-install.sh" >> /home/ubuntu/arr-provision.log
 chmod +x ./aws-ruby-rails-master/ec2-ruby-rails-config-install.sh && \
 
-echo "ec2-ruby-rails.sh : ./aws-ruby-rails-master/ec2-ruby-rails-config-install.sh" >> /home/ubuntu/arr-provision.log && \
+echo "ec2-ruby-rails.sh : ./aws-ruby-rails-master/ec2-ruby-rails-config-install.sh" >> /home/ubuntu/arr-provision.log
 ./aws-ruby-rails-master/ec2-ruby-rails-config-install.sh >> /home/ubuntu/arr-provision.log && \
 
-echo "ec2-ruby-rails.sh : rm -r ./aws-ruby-rails-master" >> /home/ubuntu/arr-provision.log && \
+echo "ec2-ruby-rails.sh : rm -r ./aws-ruby-rails-master" >> /home/ubuntu/arr-provision.log
 rm -r ./aws-ruby-rails-master && \
 
-echo "ec2-ruby-rails.sh : rm aws-ruby-rails.zip" >> /home/ubuntu/arr-provision.log && \
+echo "ec2-ruby-rails.sh : rm aws-ruby-rails.zip" >> /home/ubuntu/arr-provision.log
 rm aws-ruby-rails.zip && \
 
 echo "END ec2-ruby-rails.sh" >> /home/ubuntu/arr-provision.log
