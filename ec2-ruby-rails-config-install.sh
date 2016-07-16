10a00b33d8f17208a9492f2e174619028a48611f
Rex Addiscentis <raddiscentis@addiscent.com>
2016-07-15 18:43:20 -0700

    Use THIS_FILENAME

--- ec2-ruby-rails-config-install.sh
+++ ec2-ruby-rails-config-install.sh
@@ -2,14 +2,16 @@
 # installs software and configures an EC2 instance for use as a
 # Ruby/Rails development/education machine, or potentially usable as a
 # Rails server
-# rex 2016.0713.1240
+# rex 2016.0715.1840
 
-echo "BEGIN ec2-ruby-rails-config-install.sh" >> /home/ubuntu/arr-provision.log
+THIS_FILENAME="ec2-ruby-rails-config-install.sh"
+
+echo "BEGIN $THIS_FILENAME" >> /home/ubuntu/arr-provision.log
 
 ##############################################################################
 # set the hostname
 
-echo "ec2-ruby-rails-config-install.sh : Set hostname to aws-ruby-rails"
+echo "$THIS_FILENAME : Set hostname to aws-ruby-rails"
 
 hostname aws-ruby-rails && \
 
@@ -20,7 +22,7 @@ sed -i 's/localhost/localhost aws-ruby-rails/g' /etc/hosts && \
 #############################################################################
 # set custom prompt and functions/aliases in .bashrc for users root and ubuntu
 
-echo "ec2-ruby-rails-config-install.sh : Set .bashrc customization for root and ubuntu"
+echo "$THIS_FILENAME : Set .bashrc customization for root and ubuntu"
 
 cat ./aws-ruby-rails-master/bashrc-mod.txt >> /home/ubuntu/.bashrc && \
 
@@ -29,23 +31,23 @@ cat ./aws-ruby-rails-master/bashrc-mod.txt >> /root/.bashrc && \
 ############################################################################
 # install ruby/rails and related tools/software
 
-echo "ec2-ruby-rails-config-install.sh : wget -O ruby-rails-install.zip https://github.com/addiscent/ruby-rails-install/archive/master.zip" >> /home/ubuntu/arr-provision.log
+echo "$THIS_FILENAME : wget -O ruby-rails-install.zip https://github.com/addiscent/ruby-rails-install/archive/master.zip" >> /home/ubuntu/arr-provision.log
 wget -O ruby-rails-install.zip https://github.com/addiscent/ruby-rails-install/archive/master.zip && \
 
-echo "ec2-ruby-rails-config-install.sh : unzip ruby-rails-install.zip" >> /home/ubuntu/arr-provision.log
+echo "$THIS_FILENAME : unzip ruby-rails-install.zip" >> /home/ubuntu/arr-provision.log
 unzip ruby-rails-install.zip && \
 
-echo "ec2-ruby-rails-config-install.sh : chmod +x ./ruby-rails-install-master/ruby-rails-install.sh" >> /home/ubuntu/arr-provision.log
+echo "$THIS_FILENAME : chmod +x ./ruby-rails-install-master/ruby-rails-install.sh" >> /home/ubuntu/arr-provision.log
 chmod +x ./ruby-rails-install-master/ruby-rails-install.sh && \
 
-echo "ec2-ruby-rails-config-install.sh : ./ruby-rails-install-master/ruby-rails-install.sh" >> /home/ubuntu/arr-provision.log
+echo "$THIS_FILENAME : ./ruby-rails-install-master/ruby-rails-install.sh" >> /home/ubuntu/arr-provision.log
 ./ruby-rails-install-master/ruby-rails-install.sh >> /home/ubuntu/arr-provision.log && \
 
-echo "ec2-ruby-rails-config-install.sh : rm -r ./ruby-rails-install-master" >> /home/ubuntu/arr-provision.log
+echo "$THIS_FILENAME : rm -r ./ruby-rails-install-master" >> /home/ubuntu/arr-provision.log
 rm -r ./ruby-rails-install-master && \
 
-echo "ec2-ruby-rails-config-install.sh : rm ruby-rails-install.zip" >> /home/ubuntu/arr-provision.log
+echo "$THIS_FILENAME : rm ruby-rails-install.zip" >> /home/ubuntu/arr-provision.log
 rm ruby-rails-install.zip && \
 
-echo "END ec2-ruby-rails-config-install.sh" >> /home/ubuntu/arr-provision.log
+echo "END $THIS_FILENAME" >> /home/ubuntu/arr-provision.log
 
--- ec2-ruby-rails.sh
+++ ec2-ruby-rails.sh
@@ -1,35 +1,37 @@
 #!/bin/bash
 # installs software and configures an EC2 instance for use as a
 # Ruby/Rails development/education machine
-# rex 2016.0715.1800
+# rex 2016.0715.1830
 
-echo "BEGIN ec2-ruby-rails.sh" >> /home/ubuntu/arr-provision.log
+THIS_FILENAME="ec2-ruby-rails.sh"
+
+echo "BEGIN $THIS_FILENAME" >> /home/ubuntu/arr-provision.log
 
 ############################################################################
 # install unzip
-echo "ec2-ruby-rails-config-install.sh : apt-get -y install unzip" >> /home/ubuntu/arr-provision.log
+echo "$THIS_FILENAME : apt-get -y install unzip" >> /home/ubuntu/arr-provision.log
 apt-get -y install unzip && \
 
 ############################################################################
 # download and execute provisioning software
 
-echo "ec2-ruby-rails.sh : wget -O aws-ruby-rails.zip https://github.com/addiscent/aws-ruby-rails/archive/master.zip" >> /home/ubuntu/arr-provision.log
+echo "$THIS_FILENAME : wget -O aws-ruby-rails.zip https://github.com/addiscent/aws-ruby-rails/archive/master.zip" >> /home/ubuntu/arr-provision.log
 wget -O aws-ruby-rails.zip https://github.com/addiscent/aws-ruby-rails/archive/master.zip && \
 
-echo "ec2-ruby-rails.sh : unzip aws-ruby-rails.zip" >> /home/ubuntu/arr-provision.log
+echo "$THIS_FILENAME : unzip aws-ruby-rails.zip" >> /home/ubuntu/arr-provision.log
 unzip aws-ruby-rails.zip && \
 
-echo "ec2-ruby-rails.sh : chmod +x ./aws-ruby-rails-master/ec2-ruby-rails-config-install.sh" >> /home/ubuntu/arr-provision.log
+echo "$THIS_FILENAME : chmod +x ./aws-ruby-rails-master/ec2-ruby-rails-config-install.sh" >> /home/ubuntu/arr-provision.log
 chmod +x ./aws-ruby-rails-master/ec2-ruby-rails-config-install.sh && \
 
-echo "ec2-ruby-rails.sh : ./aws-ruby-rails-master/ec2-ruby-rails-config-install.sh" >> /home/ubuntu/arr-provision.log
+echo "$THIS_FILENAME : ./aws-ruby-rails-master/ec2-ruby-rails-config-install.sh" >> /home/ubuntu/arr-provision.log
 ./aws-ruby-rails-master/ec2-ruby-rails-config-install.sh >> /home/ubuntu/arr-provision.log && \
 
-echo "ec2-ruby-rails.sh : rm -r ./aws-ruby-rails-master" >> /home/ubuntu/arr-provision.log
+echo "$THIS_FILENAME : rm -r ./aws-ruby-rails-master" >> /home/ubuntu/arr-provision.log
 rm -r ./aws-ruby-rails-master && \
 
-echo "ec2-ruby-rails.sh : rm aws-ruby-rails.zip" >> /home/ubuntu/arr-provision.log
-rm aws-ruby-rails.zip && \
+echo "$THIS_FILENAME : rm aws-ruby-rails.zip" >> /home/ubuntu/arr-provision.log
+rm aws-ruby-rails.zip
 
-echo "END ec2-ruby-rails.sh" >> /home/ubuntu/arr-provision.log
+echo "END $THIS_FILENAME" >> /home/ubuntu/arr-provision.log
 
